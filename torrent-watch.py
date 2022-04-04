a92df253659c45a3e8a286639d5d5df67ca3bc73
cromachina <82557197+cromachina@users.noreply.github.com>
2022-04-04 16:45:03 -0400

    Add ensure_list guard around nyaa rss data

--- torrent-watch.py
+++ torrent-watch.py
@@ -37,10 +37,13 @@ class TransmissionApi():
             self.restart_session()
             self.torrent_add(torrent_url, download_location, tries - 1)
 
+def ensure_list(thing):
+    return thing if type(thing) is list else [thing]
+
 def get_torrent_data_for_show(search_string):
     response = httpx.get(nyaa_url, params={'page': 'rss', 'q': search_string})
     if response.status_code == 200:
-        return xmltodict.parse(response.text)['rss']['channel']['item']
+        return ensure_list(xmltodict.parse(response.text)['rss']['channel']['item'])
 
 def download_show(search_string, download_location, episode_start=1):
     session = TransmissionApi()
