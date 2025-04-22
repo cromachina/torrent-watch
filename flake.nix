5e78ac42179fcdfcfd0ac46efd97c8552305c373
cromachina <82557197+cromachina@users.noreply.github.com>
2025-04-21 22:15:57 -0400

    Refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -1,54 +1,33 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
-
-  outputs =
-    {
-      self,
-      nixpkgs,
-      poetry2nix,
-    }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+  flake-utils.lib.eachDefaultSystem (
+    system:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication mkPoetryEnv;
+      config = {
+        projectDir = self;
+        preferWheels = true;
+      };
     in
     {
-      packages = forAllSystems (
-        system:
-        let
-          inherit (poetry2nix.lib.mkPoetry2Nix { pkgs = pkgs.${system}; }) mkPoetryApplication;
-        in
-        {
-          default = mkPoetryApplication { projectDir = self; };
-        }
-      );
-
-      devShells = forAllSystems (
-        system:
-        let
-          inherit
-            (poetry2nix.lib.mkPoetry2Nix {
-              pkgs = pkgs.${system};
-            })
-            mkPoetryEnv
-            ;
-        in
-        {
-          default = pkgs.${system}.mkShellNoCC {
-            packages = with pkgs.${system}; [
-              (mkPoetryEnv {
-                projectDir = self;
-              })
-              poetry
-            ];
-          };
-        }
-      );
-    };
+      packages.default = mkPoetryApplication config;
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          poetry
+          (mkPoetryEnv config)
+        ];
+      };
+    }
+  );
 }
